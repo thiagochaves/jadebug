86fb975d823bb0571dced6168191bdf408336646
Thiago Chaves <8999197+thiagochaves@users.noreply.github.com>
2023-05-09 22:01:49 -0300

    Parse an initial composite event with vm start

--- jadebug/connect.py
+++ jadebug/connect.py
@@ -1,5 +1,7 @@
+from __future__ import annotations
 import socket
 from enum import Enum
+import typing
 
 
 class Connection:
@@ -17,6 +19,8 @@ def connect_to_jvm() -> Connection:
         raise Exception("Handshake failed.")
     header = deserialize_header(sck.recv(11))
     print(header)
+    package = header.deserialize(sck.recv(header.length - 11))
+    print(package)
     return Connection(sck)
 
 
@@ -32,11 +36,147 @@ class CommandSet(Enum):
 _command_sets = {64: CommandSet.EVENT}
 
 
-class Command(Enum):
-    COMPOSITE = (CommandSet.EVENT, 100)
+class Command:
+    @staticmethod
+    def deserialize(header: CommandHeader, data: bytes) -> Command:
+        return Command()
 
 
-_commands = {(CommandSet.EVENT, 100): Command.COMPOSITE}
+class Reply:
+    pass
+
+
+class CompositeCommand(Command):
+    header: CommandHeader
+    suspend_policy: ByteField
+    event_count: IntField
+    events: list[Event]
+
+    def __init__(
+        self,
+        header: CommandHeader,
+        suspend_policy: ByteField,
+        event_count: IntField,
+        events: list[Event],
+    ) -> None:
+        self.header = header
+        self.suspend_policy = suspend_policy
+        self.event_count = event_count
+        self.events = events
+
+    @staticmethod
+    def deserialize(header: CommandHeader, data: bytes) -> CompositeCommand:
+        suspend_policy = ByteField.deserialize(data[0:1])
+        event_count = IntField.deserialize(data[1:5])
+        events = []
+        for _ in range(event_count.value):
+            events.append(Event.deserialize(data[5:]))
+        return CompositeCommand(header, suspend_policy, event_count, events)
+
+    def __repr__(self):
+        return f"CompositeCommand(header={self.header}, suspend_policy={self.suspend_policy}, event_count={self.event_count}, events={self.events})"
+
+
+_commands = {(CommandSet.EVENT, 100): CompositeCommand}
+
+
+class ByteField:
+    value: int
+
+    def __init__(self, value: int) -> None:
+        self.value = value
+
+    @staticmethod
+    def deserialize(data: bytes) -> ByteField:
+        return ByteField(int.from_bytes(data[0:1], byteorder="big"))
+
+    def __repr__(self):
+        return f"ByteField(value={self.value})"
+
+
+class IntField:
+    value: int
+
+    def __init__(self, value: int) -> None:
+        self.value = value
+
+    @staticmethod
+    def deserialize(data: bytes) -> IntField:
+        return IntField(int.from_bytes(data[0:4], byteorder="big"))
+
+    def __repr__(self):
+        return f"IntField(value={self.value})"
+
+
+class Event:
+    @staticmethod
+    def deserialize(data: bytes) -> Event:
+        event_kind = ByteField.deserialize(data[0:1])
+        event = _event_kinds[event_kind.value]
+        return event.deserialize(data[1:])
+
+
+class EventVmStart(Event):
+    request_id: IntField
+    thread: ThreadId
+
+    def __init__(self, request_id: IntField, thread: ThreadId) -> None:
+        self.request_id = request_id
+        self.thread = thread
+
+    @staticmethod
+    def deserialize(data: bytes) -> EventVmStart:
+        request_id = IntField.deserialize(data[0:4])
+        thread = ThreadId.deserialize(data[4:])
+        return EventVmStart(request_id, thread)
+
+    def __repr__(self):
+        return f"EventVmStart(request_id={self.request_id}, thread={self.thread})"
+
+
+class ThreadId:
+    value: int
+
+    def __init__(self, value: int) -> None:
+        self.value = value
+
+    @staticmethod
+    def deserialize(data: bytes) -> ThreadId:
+        return ThreadId(int.from_bytes(data[0:8], byteorder="big"))
+
+    def __repr__(self):
+        return f"ThreadId(value={self.value})"
+
+
+class EventKind(Enum):
+    SINGLE_STEP = 1
+    BREAKPOINT = 2
+    FRAME_POP = 3
+    EXCEPTION = 4
+    USER_DEFINED = 5
+    THREAD_START = 6
+    THREAD_DEATH = 7
+    THREAD_END = 7
+    CLASS_PREPARE = 8
+    CLASS_UNLOAD = 9
+    CLASS_LOAD = 10
+    FIELD_ACCESS = 20
+    FIELD_MODIFICATION = 21
+    EXCEPTION_CATCH = 30
+    METHOD_ENTRY = 40
+    METHOD_EXIT = 41
+    METHOD_EXIT_WITH_RETURN_VALUE = 42
+    MONITOR_CONTENDED_ENTER = 43
+    MONITOR_CONTENDED_ENTERED = 44
+    MONITOR_WAIT = 45
+    MONITOR_WAITED = 46
+    VM_START = 90
+    VM_INIT = 90
+    VM_DEATH = 99
+    VM_DISCONNECTED = 100
+
+
+_event_kinds = {EventKind.VM_START.value: EventVmStart}
 
 
 class Header:
@@ -46,6 +186,9 @@ class Header:
     def __init__(self, length: int, id: int) -> None:
         self.length = length
         self.id = id
+
+    def deserialize(self, data: bytes) -> typing.Union[Reply, Command]:
+        return Command()
 
 
 class ReplyHeader(Header):
@@ -58,10 +201,13 @@ class ReplyHeader(Header):
     def __repr__(self):
         return f"ReplyHeader(length={self.length}, id={self.id}, code={self.code})"
 
+    def deserialize(self, data: bytes) -> Reply:
+        return Reply()
+
 
 class CommandHeader(Header):
     cmd_set: CommandSet
-    cmd: Command
+    cmd: typing.Type[Command]
 
     def __init__(self, length: int, id: int, cmd_set: int, cmd: int) -> None:
         super().__init__(length, id)
@@ -70,6 +216,9 @@ class CommandHeader(Header):
 
     def __repr__(self):
         return f"CommandHeader(length={self.length}, id={self.id}, cmd_set={self.cmd_set}, cmd={self.cmd})"
+
+    def deserialize(self, data: bytes) -> Command:
+        return self.cmd.deserialize(self, data)
 
 
 def deserialize_header(data: bytes) -> Header:
